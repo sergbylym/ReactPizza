7b755d86c5000d9f0e8b09ed7619ea55c6e5d925
Serhii <sergbylym@gmail.com>
2022-12-22 21:27:09 +0200

    create map for categoties

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,49 +1,21 @@
 import React from "react";
 const Categories = () => {
   const [activeIndex, setActiveIndex] = React.useState(0);
-
+  const categories = ["All", "Meet", "Vegetariano", "Grill", "Hot"];
   const onClickCategory = (index) => {
     setActiveIndex(index);
   }; //mark  selected category
   return (
     <div className="categories">
       <ul>
-        <li
-          onClick={() => onClickCategory(0)}
-          className={activeIndex === 0 ? 'active' : ""}
-        >
-          All
-        </li>
-        <li
-          onClick={() => onClickCategory(1)}
-          className={activeIndex === 1 ? 'active' : ""}
-        >
-          Meet
-        </li>
-        <li
-          onClick={() => onClickCategory(2)}
-          className={activeIndex === 2 ? 'active' : ""}
-        >
-          Vegetariano
-        </li>
-        <li
-          onClick={() => onClickCategory(3)}
-          className={activeIndex === 3 ? 'active' : ""}
-        >
-          Grill
-        </li>
-        <li
-          onClick={() => onClickCategory(4)}
-          className={activeIndex === 4 ? 'active' : ""}
-        >
-          Hot
-        </li>
-        <li
-          onClick={() => onClickCategory(5)}
-          className={activeIndex === 5 ? 'active' : ""}
-        >
-          Close
-        </li>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
