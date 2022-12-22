ca7c921cda858af36282529d488056dc27bffcac
Serhii <sergbylym@gmail.com>
2022-12-22 16:56:05 +0200

    add onClick for Category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,52 @@
+import React from "react";
+const Categories = () => {
+  const [activeIndex, setActiveIndex] = React.useState(0);
 
-const Categories =() => {
-   return (
-      <div className="categories">
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+  return (
+    <div className="categories">
       <ul>
-        <li className="active">All</li>
-        <li>Meet</li>
-        <li>Vegetariano</li>
-        <li>Grill</li>
-        <li>Hot</li>
-        <li>Close</li>
+        <li
+          onClick={() => onClickCategory(0)}
+          className={activeIndex === 0 ? 'active' : ""}
+        >
+          All
+        </li>
+        <li
+          onClick={() => onClickCategory(1)}
+          className={activeIndex === 1 ? 'active' : ""}
+        >
+          Meet
+        </li>
+        <li
+          onClick={() => onClickCategory(2)}
+          className={activeIndex === 2 ? 'active' : ""}
+        >
+          Vegetariano
+        </li>
+        <li
+          onClick={() => onClickCategory(3)}
+          className={activeIndex === 3 ? 'active' : ""}
+        >
+          Grill
+        </li>
+        <li
+          onClick={() => onClickCategory(4)}
+          className={activeIndex === 4 ? 'active' : ""}
+        >
+          Hot
+        </li>
+        <li
+          onClick={() => onClickCategory(5)}
+          className={activeIndex === 5 ? 'active' : ""}
+        >
+          Close
+        </li>
       </ul>
     </div>
-   )
-}
+  );
+};
 
 export default Categories;
