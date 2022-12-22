69ef00db44501a05d135b9a3fede8b76f5f936bf
Serhii <sergbylym@gmail.com>
2022-12-22 16:38:06 +0200

    add assets/img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
+import PizzaLogo from '../assets/img/pizza-logo.svg'
+
 const Header = () => {
   return (
     <div className="header">
       <div className="container">
         <div className="header__logo">
-          <img width="38" src="/img/pizza-logo.svg" alt="Pizza logo" />
+          <img width="38" src={PizzaLogo} alt="Pizza logo" />
           <div>
             <h1>React Pizza</h1>
-            <p>самая вкусная пицца во вселенной</p>
+            <p>THE BEST DELICIOUS PIZZA</p>
           </div>
         </div>
         <div className="header__cart">
