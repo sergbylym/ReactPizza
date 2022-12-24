f417deb7b4f2589c62a479dffddd2ce3b0a3cce1
Serhii <sergbylym@gmail.com>
2022-12-24 13:33:25 +0200

    make backend pizza data + create useState for item from backend + add fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react'
 import "../src/scss/app.scss";
 import Categories from "./components/Categories";
 import Header from "./components/Header";
 import PizzaBlock from "./components/PizzaBlock";
 import Sort from "./components/Sort";
-import pizzas from "./assets/pizzas.json";
+// import pizzas from "./assets/pizzas.json";
 function App() {
+  let [items, setItems] = React.useState([])
+  fetch('https://63a6dada59fd83b1bb393cd1.mockapi.io/items')
+  .then((res) => { 
+    return res.json()})
+  .then((arr) => {
+   setItems(arr)
+  })
   return (
     <div className="App">
       <div className="wrapper">
@@ -17,7 +25,7 @@ function App() {
             </div>
             <h2 className="content__title">All pizza</h2>
             <div className="content__items">
-              {pizzas.map((obj) => (
+              {items.map((obj) => (
                 <PizzaBlock  key={obj.id} {...obj}/>
               ))}
             </div>
