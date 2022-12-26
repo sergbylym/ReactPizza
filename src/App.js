9e8694e9b561781caf359ea42f4bfd8c8093dac0
Serhii <sergbylym@gmail.com>
2022-12-26 13:36:17 +0200

    implement functional for Skeleton

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import "../src/scss/app.scss";
 import Categories from "./components/Categories";
 import Header from "./components/Header";
 import PizzaBlock from "./components/PizzaBock";
+import Skeleton from "./components/PizzaBock/Skeleton";
 import Sort from "./components/Sort";
 // import pizzas from "./assets/pizzas.json";// files for render pizza without backend
 function App() {
   let [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true)
+
   React.useEffect(() => {
     fetch("https://63a6dada59fd83b1bb393cd1.mockapi.io/items")
       .then(res =>  res.json())
       .then((arr) => {
         setItems(arr);
+        setIsLoading(false)
       });
   }, []);
   return (
@@ -26,9 +30,16 @@ function App() {
             </div>
             <h2 className="content__title">All pizza</h2>
             <div className="content__items">
-              {items.map((obj) => (
-                <PizzaBlock key={obj.id} {...obj} />
-              ))}
+            
+            
+        {
+          isLoading 
+          ? [...new Array(6)].map((_, index) => <Skeleton key = {index}/>)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj}/>)
+        }
+                           
+
+
             </div>
           </div>
         </div>
