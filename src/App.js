8ede47ee49c67856f2c0d00952d85f19c70fa708
Serhii <sergbylym@gmail.com>
2022-12-26 13:58:47 +0200

    create pages and add content for Home pag

--- src/App.js
+++ src/App.js
@@ -1,47 +1,16 @@
 import React from "react";
 import "../src/scss/app.scss";
-import Categories from "./components/Categories";
 import Header from "./components/Header";
-import PizzaBlock from "./components/PizzaBock";
-import Skeleton from "./components/PizzaBock/Skeleton";
-import Sort from "./components/Sort";
+import Home from "./pages/Home";
 // import pizzas from "./assets/pizzas.json";// files for render pizza without backend
 function App() {
-  let [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true)
-
-  React.useEffect(() => {
-    fetch("https://63a6dada59fd83b1bb393cd1.mockapi.io/items")
-      .then(res =>  res.json())
-      .then((arr) => {
-        setItems(arr);
-        setIsLoading(false)
-      });
-  }, []);
+  
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         <div className="content">
-          <div className="container">
-            <div className="content__top">
-              <Categories />
-              <Sort />
-            </div>
-            <h2 className="content__title">All pizza</h2>
-            <div className="content__items">
-            
-            
-        {
-          isLoading 
-          ? [...new Array(6)].map((_, index) => <Skeleton key = {index}/>)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj}/>)
-        }
-                           
-
-
-            </div>
-          </div>
+         <Home/>
         </div>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Categories from "../components/Categories"
+import Sort from "../components/Sort"
+import Skeleton from "../components/PizzaBock/Skeleton"
+import PizzaBlock from "../components/PizzaBock"
+const Home = () => {
+   let [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    fetch("https://63a6dada59fd83b1bb393cd1.mockapi.io/items")
+      .then(res =>  res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false)
+      });
+  }, []);
+   return (
+   <>
+   <div className="container">
+<div className="content__top">
+  <Categories />
+  <Sort />
+</div>
+<h2 className="content__title">All pizza</h2>
+<div className="content__items">
+
+
+{
+isLoading 
+? [...new Array(6)].map((_, index) => <Skeleton key = {index}/>)
+: items.map((obj) => <PizzaBlock key={obj.id} {...obj}/>)
+}
+               
+
+
+</div>
+</div>
+</>
+   )
+}
+
+export default Home;
