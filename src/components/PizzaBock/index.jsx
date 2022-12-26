785723c60c80118b1efbbda3e5a777663f30c096
Serhii <sergbylym@gmail.com>
2022-12-26 17:31:12 +0200

    add pizzablock wrapper and scss style

--- src/components/PizzaBock/index.jsx
+++ src/components/PizzaBock/index.jsx
@@ -5,6 +5,7 @@ const PizzaBlock = ({ title, price, imageUrl, sizes, types }) => {
   const [activeType, setActiveType] = React.useState(0)
   const [activeSize, setActiveSize] = React.useState(0)
   return (
+    <div className="pizza-block-wrapper">
     <div className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
@@ -40,6 +41,7 @@ const PizzaBlock = ({ title, price, imageUrl, sizes, types }) => {
           <i>0</i>
         </button>
       </div>
+    </div>
     </div>
   );
 };
