24e155c50f8b7a1308a7bafeecf4e223a303952e
Serhii <sergbylym@gmail.com>
2022-12-26 16:59:32 +0200

    create route for Cart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "../src/scss/app.scss";
 import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 // import pizzas from "./assets/pizzas.json";// files for render pizza without backend
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/cart" element={<Cart/>}/>
             <Route path="*" element={<NotFound/>}/>
           </Routes>
         </div>
