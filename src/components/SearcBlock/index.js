7aae8e91a7e7c43b7abf3c6a8df486fb4b78c398
Serhii <sergbylym@gmail.com>
2023-01-06 10:29:04 +0200

    add value for input and fix syle for input

--- src/components/SearcBlock/index.js
+++ src/components/SearcBlock/index.js
@@ -3,9 +3,7 @@ const Search = ({ searchValue, setSearchValue }) => {
   return (
     <div className={style.root}>
       <svg
-        value={searchValue}
         className={style.icon}
-        enable-background="new 0 0 32 32"
         id="Glyph"
         version="1.1"
         viewBox="0 0 32 32"
@@ -17,20 +15,24 @@ const Search = ({ searchValue, setSearchValue }) => {
         />
       </svg>
       <input
+        value={searchValue}
         onChange={(event) => setSearchValue(event.target.value)}
         className={style.input}
         placeholder="Search pizza..."
       />
-      <svg
-      className={style.clearIcon}
-        height="48"
-        viewBox="0 0 48 48"
-        width="48"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M38 12.83L35.17 10 24 21.17 12.83 10 10 12.83 21.17 24 10 35.17 12.83 38 24 26.83 35.17 38 38 35.17 26.83 24z" />
-        <path d="M0 0h48v48H0z" fill="none" />
-      </svg>
+      {searchValue && (
+        <svg
+          onClick={() => setSearchValue("")}
+          className={style.clearIcon}
+          height="48"
+          viewBox="0 0 48 48"
+          width="48"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M38 12.83L35.17 10 24 21.17 12.83 10 10 12.83 21.17 24 10 35.17 12.83 38 24 26.83 35.17 38 38 35.17 26.83 24z" />
+          <path d="M0 0h48v48H0z" fill="none" />
+        </svg>
+      )}
     </div>
   );
 };
