082b071896d1c886369a3f086ba1a006431be3b4
Serhii <sergbylym@gmail.com>
2023-01-09 15:07:54 +0200

    style for Pagnaition

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import ReactPaginate from 'react-paginate'
-import style from './Pagination.module.scss'
+import React from "react";
+import ReactPaginate from "react-paginate";
+import style from "./Pagination.module.scss";
 const Pagination = () => {
   return (
-   <ReactPaginate
-   className={style.root}
-   breakLabel="..."
-   nextLabel=">"
-   onPageChange={(event) => console.log(event)}
-   pageRangeDisplayed={8}
-   pageCount={3}
-   previousLabel="<"
-   renderOnZeroPageCount={null}
- />
-  )
-}
+    <ReactPaginate
+      className={style.root}
+      breakLabel="..."
+      nextLabel=">"
+      onPageChange={(event) => console.log(event)}
+      pageRangeDisplayed={8}
+      pageCount={3}
+      previousLabel="<"
+      renderOnZeroPageCount={null}
+    />
+  );
+};
 
-
-export default Pagination
+export default Pagination;
