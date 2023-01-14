627e7ce83dee044b669a693fd405cdffe4390015
Serhii <sergbylym@gmail.com>
2023-01-14 12:21:26 +0200

    create filterSlice reducer

--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -1,9 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
-   categoryId: 0,
+  categoryId: 0,
    sort: {
-      name: {
-         name: "Popular",
-         sortType: "rating",
-       }
-   }
-}
+    name: {
+      name: "Popular",
+      sortType: "rating",
+    },
+  },
+};
+
+const filterSlice = createSlice({
+  name: "filters",
+  initialState,
+  reducers: {
+    setCategoryID(state, action) {
+      state.categoryId = action.payload;
+    },
+  },
+});
+
+
+export const {setCategoryID} = filterSlice.actions
+export default filterSlice.reducer;
