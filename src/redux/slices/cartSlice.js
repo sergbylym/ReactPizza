8bc385bb8f2eb8d064ea3a51003b5deb1061b4df
Serhii <sergbylym@gmail.com>
2023-02-02 14:00:17 +0200

    create state and method for cartSlice

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+ totalPrice: 0,
+ items: [],
+}
+
+
+
+
+const cartSlice = createSlice ({
+name: 'cart',
+initialState,
+reducers: {
+   addProduct(state, action) {
+state.items.push(action.payload)
+   }
+}
+})
+export const {addProduct} = cartSlice.actions;
+
+export default cartSlice.reducer
+
+
